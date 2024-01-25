4f4df582909542ea2997a15d1d1f5a133551216c
Mel-Hart-coder <142450309+Mel-Hart-coder@users.noreply.github.com>
2024-01-25 13:41:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 5500;
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Express')
+  res.send('<h1>I am an Enneagram Type 6: The Loyalist</h1> <p>The pros to working with a type 6 is that they are devoted to the project</p><p>The cons of working with a type 6 is that they are risk averse which can stunt a project. </p>')
 })
 
 
