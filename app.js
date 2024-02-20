3f5a9a6a454b88e89f3c0ad593096788d8b48fd2
Melissa Hart <melissahart@melissas-air.main.local.una.edu>
2024-02-20 15:33:03 -0600

    hello

--- app.js
+++ app.js
@@ -53,11 +53,32 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+   const results = await client.db("papa-lab").collection("projects").find().toArray();
+    
+    
+    return results;
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
-run().catch(console.dir);
+// run().catch(console.dir);
+
+
+app.get('/read', async(reg,res) => {
+  
+  let myResultServer = await run();
+  console.log(myResultServer[0].name);
+
+
+
+  res.render('index', {
+   
+    myClientType: myTypeServer, 
+    myResultClient: myResultServer
+
+  });
+  
+
+});
